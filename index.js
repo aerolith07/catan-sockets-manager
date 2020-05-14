19bd0169137614ae85639eeeb4b188ae144be5a0
bagejar <rahul.bageja@ig.com>
2020-05-14 18:02:31 +0100

    added skeleton sockets code

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4545
+
+const app = express()
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected", socket.id.substr(0,6));
+
+  // When update recieved from one client, emit an update to all other clients
+  socket.on("apiUpdateLocal", (data) => {
+    console.log(`local update [${data}] recieved, sending to everyone else`)
+    socket.broadcast.emit('apiUpdate')
+  });
+
+  socket.on("disconnect", () => {
+    console.log('Disconnected', socket.id.substr(0,6))
+  });
+});
+
+server.listen(port, () => console.log(`listening for sockets on port ${port}`))
